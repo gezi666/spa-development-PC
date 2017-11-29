79f19cb4aa6780e3744a1065988ae80b364fa09f
weibin <weibsgz@126.com>
2017-11-29 18:26:04 +0800

    read me

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home-wrap">
 
+
+    <a target="_blank" href="https://github.com/jusfoun-FE/spa-development-PC/blob/master/README.md">VUE-PC模板使用说明</a>
+
+      <div class="theme-wrapper">
         <theme></theme>
-        <h3> 换肤相关说明：</h3>
-        <p>1.components/theme/theme.vue下是换肤组件 在任何地方都可以引入</p>
-        <p>2.通过vuex将选中的颜色class存储,在app.vue中通过改变根组件的class名实现换肤</p>
-        <p>3.已加入本地存储 刷新页面后仍保留上次选中的皮肤</p>
+      </div>
+
+
 
   </div>
 
@@ -32,5 +35,17 @@
 <style lang="scss" scoped>
   @import "~assets/css/variable";
   @import "~assets/css/mixin";
+
+  .home-wrap{
+    padding: 20px;
+    a{
+      text-decoration: underline;
+      color: #F17470;
+      font-size: 20px;
+    }
+    .theme-wrapper{
+      float: right;
+    }
+  }
 
 </style>
